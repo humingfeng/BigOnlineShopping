bc9615c5b2c6a87f462d7589f2de6ad4b6172bad
humingfeng <humf@vip.qq.com>
2018-05-22 16:55:30 +0800

    进度提交

--- bos-manager/bos-manager-interface/src/main/java/cn/bos/service/ItemService.java
+++ bos-manager/bos-manager-interface/src/main/java/cn/bos/service/ItemService.java
@@ -1,10 +1,13 @@
 package cn.bos.service;
 
 import cn.bos.common.pojo.EasyUIDataGridResult;
+import cn.bos.common.utils.E3Result;
 import cn.bos.pojo.TbItem;
 
 public interface ItemService {
     public TbItem getItemById(long itemId);
 
     public EasyUIDataGridResult getItemList(int page, int rows);
+
+    public E3Result addItem(TbItem item, String desc);
 }
--- bos-manager/bos-manager-service/src/main/java/cn/bos/service/impl/ItemServiceImpl.java
+++ bos-manager/bos-manager-service/src/main/java/cn/bos/service/impl/ItemServiceImpl.java
@@ -1,8 +1,13 @@
 package cn.bos.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import cn.bos.common.pojo.EasyUIDataGridResult;
+import cn.bos.common.utils.E3Result;
+import cn.bos.common.utils.IDUtils;
+import cn.bos.mapper.TbItemDescMapper;
+import cn.bos.pojo.TbItemDesc;
 import cn.bos.service.ItemService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -28,6 +33,9 @@ public class ItemServiceImpl implements ItemService {
 
     @Autowired
     private TbItemMapper itemMapper;
+
+    @Autowired
+    private TbItemDescMapper itemDescMapper;
 
     @Override
     public TbItem getItemById(long itemId) {
@@ -71,6 +79,33 @@ public class ItemServiceImpl implements ItemService {
 
 
         return result;
+    }
+
+    @Override
+    public E3Result addItem(TbItem item, String desc) {
+        // 1、生成商品id
+        long itemId = IDUtils.genItemId();
+        // 2、补全TbItem对象的属性
+        item.setId(itemId);
+        //商品状态，1-正常，2-下架，3-删除
+        item.setStatus((byte) 1);
+        Date date = new Date();
+        item.setCreated(date);
+        item.setUpdated(date);
+        // 3、向商品表插入数据
+        itemMapper.insert(item);
+        // 4、创建一个TbItemDesc对象
+        TbItemDesc itemDesc = new TbItemDesc();
+        // 5、补全TbItemDesc的属性
+        itemDesc.setItemId(itemId);
+        itemDesc.setItemDesc(desc);
+        itemDesc.setCreated(date);
+        itemDesc.setUpdated(date);
+        // 6、向商品描述表插入数据
+        itemDescMapper.insert(itemDesc);
+        // 7、E3Result.ok()
+        return E3Result.ok();
+
     }
 
 }
